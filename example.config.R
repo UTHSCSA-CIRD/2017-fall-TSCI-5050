b2dca1e66e1ea085bed36a8a7b6314e300f30e39
bokov@uthscsa.edu <bokov@uthscsa.edu>
2017-09-02 16:31:08 -0500

    Initial commit. Welcome to TSCI 5050 everyone!

---
+++ example.config.R
@@ -0,0 +1,10 @@
+#' My Configuration
+#'
+# My various directories:
+ 
+homedir <- '/home/a/';
+desktop <- '/Users/a/Desktop/';
+downloads <- '/Users/a/Downloads/';
+docs <- '/Users/a/Documents/';
+temp <- '/tmp/';
+tsci5050 <- '/Users/a/Documents/17_tsci5050/';
